af868c8bd858afbefc6b11ec5042edcfa2567a11
Ivan Stoyanov <ivan.sstoyanov@abv.bg>
2019-04-07 12:11:17 +0300

    added vectors in Products

--- lab1/src/invoice.cpp
+++ lab1/src/invoice.cpp
@@ -1,8 +1,9 @@
 #include "invoice.h"
 void Invoice::add(Product product, int quantity){
-
+    Item item(product, quantity)
+    this->products.push_back(product);
 }
 
 vector<Product> Invoice:: getProducts(){
-    return vector<Product>();
+    return this->products;
 }
